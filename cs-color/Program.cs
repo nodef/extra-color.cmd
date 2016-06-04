e2985396305b1eee66857c785f740ca7ed5e2c13
Subhajit Sahu <wolfram77@gmail.com>
2016-06-04 23:15:14 +0530

    program: cute comments added to getcode

--- cs-color/Program.cs
+++ cs-color/Program.cs
@@ -41,6 +41,13 @@ namespace orez.color {
 			}
 		}
 
+		/// <summary>
+		/// Get color code from specified value string.
+		/// Value string can start with a '+' or '-' to indicate change.
+		/// </summary>
+		/// <param name="val">Color value string (ex- "3", "-2" or "green")</param>
+		/// <param name="old">Old color code, which is used as origin for change.</param>
+		/// <returns>Integer color code.</returns>
 		private static int GetCode(string val, int old) {
 			int code = 0;
 			val = (val.Length == 0 ? "+0" : val).ToLower();
@@ -48,14 +55,6 @@ namespace orez.color {
 			int.TryParse(val, out code);
 			if(val[0] == '+' || val[0] == '-') code += old;
 			return code;
-		}
-
-		private static int GetCode(string val) {
-			int ans = 0;
-			val = (val.Length == 0 ? "0" : val).ToLower();
-			if(char.IsDigit(val[0])) int.TryParse(val, out ans);
-			else ans = CodeMap.ContainsKey(val) ? CodeMap[val] : ans;
-			return ans;
 		}
 
 		private static ConsoleColor Color(int code) {
